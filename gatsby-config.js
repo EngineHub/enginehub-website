4ab9977be8e4445cb79ca237c5b2d4bba858d136
Matthew Miller <mnmiller1@me.com>
2019-06-13 23:21:47 +1000

    Update gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,12 @@ module.exports = {
         icon: `src/images/enginehub-logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      options: {
+        siteUrl: `https://enginehub.org`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
