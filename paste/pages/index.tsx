f5d8bf6c75120d4d20466182c701df5187540556
Matthew Miller <mnmiller1@me.com>
2019-07-13 00:19:25 +1000

    Try getting a serverless NextJS app to actually deploy

---
+++ paste/pages/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+
+const Index: FunctionComponent = () => {
+    return <h1>EngineHub Pastebin Service is currently under maintenance. If you're trying to upload logs for a support request, upload the file straight to Discord.</h1>;
+}
+
+export default Index;
