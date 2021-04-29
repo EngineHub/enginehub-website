69dedb97f579930e7b73a6a94861cf0c562b8348
Matthew Miller <mnmiller1@me.com>
2021-04-29 21:26:42 +1000

    chore: bump deps

--- paste/next.config.js
+++ paste/next.config.js
@@ -1,29 +1,31 @@
 const withImages = require('next-images');
 const withTM = require('next-transpile-modules')(['shared']);
 
-module.exports = withTM(withImages({
-    target: 'serverless',
-    distDir: 'build',
-    env: {
-        GA_TRACKING_ID: 'UA-139849956-4'
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/paste',
-                destination: '/api/paste'
-            },
-            {
-                source: '/signed_paste',
-                destination: '/api/signed_paste'
-            },
-            {
-                source: '/documents/:slug',
-                destination: '/api/documents/:slug'
-            }
-        ]
-    },
-    experimental: {
-        sprFlushToDisk: false
-    }
-}));
+module.exports = withTM(
+    withImages({
+        target: 'serverless',
+        distDir: 'build',
+        env: {
+            GA_TRACKING_ID: 'UA-139849956-4'
+        },
+        async rewrites() {
+            return [
+                {
+                    source: '/paste',
+                    destination: '/api/paste'
+                },
+                {
+                    source: '/signed_paste',
+                    destination: '/api/signed_paste'
+                },
+                {
+                    source: '/documents/:slug',
+                    destination: '/api/documents/:slug'
+                }
+            ];
+        },
+        experimental: {
+            sprFlushToDisk: false
+        }
+    })
+);
