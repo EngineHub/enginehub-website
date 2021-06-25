e4d57dc2d1fe6c71d80bd8faaf554a7723a1be4c
Madeline Miller <mnmiller1@me.com>
2021-06-25 19:48:34 +1000

    Invert the two plugins

--- builds/next.config.js
+++ builds/next.config.js
@@ -1,8 +1,8 @@
 const withImages = require('next-images');
 const withTM = require('next-transpile-modules')(['shared']);
 
-module.exports = withTM(
-    withImages({
+module.exports = withImages(withTM(
+    {
         env: {
             GA_TRACKING_ID: 'UA-139849956-5'
         }
