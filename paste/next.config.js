926ac2c032ac059a665fdc3169dc885512c88ab2
Madeline Miller <mnmiller1@me.com>
2022-06-20 20:19:56 +1000

    chore(paste): improve cors setup

--- paste/next.config.js
+++ paste/next.config.js
@@ -18,6 +18,26 @@ module.exports = {
             }
         ];
     },
+    async headers() {
+        return [
+            {
+                // matching signed paste route
+                source: '/api/signed_paste',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET'
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-paste-meta-name, x-paste-meta-extension, x-paste-meta-from, x-paste-meta-author'
+                    }
+                ]
+            }
+        ];
+    },
     compiler: {
         styledComponents: true
     }
